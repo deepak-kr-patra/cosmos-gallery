6bd07a8628003adec43c38d7665a985fdd9693ae
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2024-06-24 16:35:04 +0530

    updated pic section dimensions

--- src/components/PicOfTheDay.jsx
+++ src/components/PicOfTheDay.jsx
@@ -21,11 +21,11 @@ const PicOfTheDay = () => {
 
     return (
         <>
-            <div className='w-full h-[15%] flex items-center justify-center'>
+            <div className='w-full h-[10%] flex items-center justify-center'>
                 <h3 className='text-white text-[5vmin]'>NASA Picture Of The Day</h3>
             </div>
 
-            <div className={`${sectionWidth} h-[85%] flex flex-col items-center justify-center gap-4`}>
+            <div className={`${sectionWidth} h-[90%] flex flex-col items-center justify-center gap-4`}>
                 <Routes>
                     <Route path="/" element={<PotdContent />} />
                     <Route path="pic/*" element={date ? <PicSection date={date} /> : <Navigate to="/explore/potd" />} />
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -13,13 +13,8 @@ import { IoArrowBack } from "react-icons/io5";
 
 
 const Explore = () => {
-
-    const { screenWidth } = useScreenWidth();
-
-    const boxWidth = screenWidth > 1100 ? "w-[60%]" : screenWidth <= 1100 && screenWidth > 500 ? "w-[80%]" : "w-[90%]";
-
+    
     const navigate = useNavigate();
-
     const { pathname } = useLocation();
     
     const goBack = () => {
@@ -32,10 +27,15 @@ const Explore = () => {
         }
     }
 
+    const { screenWidth } = useScreenWidth();
+
+    const boxWidth = screenWidth > 1100 ? "w-[60%]" : screenWidth <= 1100 && screenWidth > 500 ? "w-[80%]" : "w-[90%]";
+    const padding = screenWidth < 500 ? "p-4" : "p-8";
+
     return (
         <div className='h-full w-full bg-gray-400 rounded-none bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 flex items-center justify-center
         '>
-            <div className={`relative ${boxWidth} h-[90%] flex flex-col items-center justify-center bg-[#1A103D] rounded-md p-8`}>
+            <div className={`relative ${boxWidth} h-[90%] flex flex-col items-center justify-center bg-[#1A103D] rounded-md ${padding}`}>
                 <div className='absolute top-0 left-0 p-2'>
                     <button className='text-white text-[4.5vmin] back-button' onClick={() => goBack()}><IoArrowBack /></button>
                 </div>
