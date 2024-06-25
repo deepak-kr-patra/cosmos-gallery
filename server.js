4ef7493e3f7dd9bd5f8b273c48c52492d3cc1cc2
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2024-06-25 21:46:38 +0530

    added an environment variable

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import path from 'path'
+import dotenv from 'dotenv'
 
-const app = express()
-const port = 5000
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
@@ -10,8 +14,8 @@ app.use(express.static(path.join(__dirname, "/dist")));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
